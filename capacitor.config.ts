4e5b1fab0791a5f0120299075366c39a3461a0b7
Álvaro <alvarora12@gmail.com>
2022-04-30 16:31:49 +0200

    Add app resources

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.arial.vocabulaire',
   appName: 'Vocabulaire',
   webDir: 'www',
   bundledWebRuntime: false
