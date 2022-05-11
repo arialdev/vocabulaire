e98145a0291634efc9ee27d29b9bca51fa2c5c5c
Álvaro <alvarora12@gmail.com>
2022-05-11 23:25:59 +0200

    Remove empty style sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import {ScreenOrientation} from '@awesome-cordova-plugins/screen-orientation/ngx
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(private settingsService: SettingsService, private screenOrientation: ScreenOrientation) {
